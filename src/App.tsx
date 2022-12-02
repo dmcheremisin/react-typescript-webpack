e7666e749af625d11c93ad8e4b0db641187a9c62
Dmitry Cheremisin <Dmitry.Cheremisin@infobip.com>
2022-12-02 17:20:24 +0300

    eslint configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './styles.css'
 import IMAGE from './react.png'
 import LOGO from './logo.svg'
+import { ClickCounter } from './ClickCounter';
 
 export const App = () => {
+    // const num = 0; eslint error - unused variable
+
     return <>
         <h1>React Typescript Webpack Starter Template</h1>
         <img src={IMAGE} alt="React Logo" width="300" height="200"/>
         <img src={LOGO} alt="React Logo" width="300" height="200"/>
         <p>Environment: {process.env.NODE_ENV}</p>
         <p>Env variable: {process.env.name}</p>
+        <ClickCounter />
     </>
 }
