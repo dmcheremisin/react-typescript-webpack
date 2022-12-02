d9949a2b3dc26073289fcd4b86e15a032243a478
Dmitry Cheremisin <Dmitry.Cheremisin@infobip.com>
2022-12-02 17:35:26 +0300

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import './styles.css'
 import IMAGE from './react.png'
 import LOGO from './logo.svg'
-import { ClickCounter } from './ClickCounter';
+import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
-    // const num = 0; eslint error - unused variable
+  // const num = 0; eslint error - unused variable
 
-    return <>
-        <h1>React Typescript Webpack Starter Template</h1>
-        <img src={IMAGE} alt="React Logo" width="300" height="200"/>
-        <img src={LOGO} alt="React Logo" width="300" height="200"/>
-        <p>Environment: {process.env.NODE_ENV}</p>
-        <p>Env variable: {process.env.name}</p>
-        <ClickCounter />
+  return (
+    <>
+      <h1>React Typescript Webpack Starter Template</h1>
+      <img src={IMAGE} alt="React Logo" width="300" height="200" />
+      <img src={LOGO} alt="React Logo" width="300" height="200" />
+      <p>Environment: {process.env.NODE_ENV}</p>
+      <p>Env variable: {process.env.name}</p>
+      <ClickCounter />
     </>
+  )
 }
