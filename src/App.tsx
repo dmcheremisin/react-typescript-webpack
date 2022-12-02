ea55b53b6eb4bea99a7c0b9f8ef74de3aa685ca5
Dmitry Cheremisin <Dmitry.Cheremisin@infobip.com>
2022-12-02 17:44:08 +0300

    pre commit hooks - husky and lint

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import LOGO from './logo.svg'
 import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
-  // const num = 0; eslint error - unused variable
+  const num = 0 // eslint error - unused variable
 
   return (
     <>
-      <h1>React Typescript Webpack Starter Template</h1>
+      <h1>React Typescript Webpack Starter Template {num}</h1>
       <img src={IMAGE} alt="React Logo" width="300" height="200" />
       <img src={LOGO} alt="React Logo" width="300" height="200" />
       <p>Environment: {process.env.NODE_ENV}</p>
